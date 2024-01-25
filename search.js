58d89a649a84068d6d255fb9b1d5332e30b20fc5
Paul <paul.cornille-avot@supinfo.com>
2024-01-25 19:43:12 +0100

    fonction de search

--- search.js
+++ search.js
@@ -4,59 +4,42 @@ document.addEventListener('DOMContentLoaded', function () {
     const lastPageButton = document.getElementById('ex_03_last_page');
     let currentPage = 1;
 
-    function fetchSwapiData(page) {
+    function fetchOmdbData(page) {
         document.getElementById('loader').style.display = "block";
         swCharList.innerHTML = '';
-        return fetch(`https://swapi.dev/api/people/?page=${page}`)
+        return fetch(`http://www.omdbapi.com/?s=star wars&page=${page}&apikey=c5ea3601`)
             .then(response => response.json());
     }
 
-    function displayCharacters(characters) {
+    function displayMovies(movies) {
         swCharList.innerHTML = '';
-        characters.results.forEach(character => {
-            const charElement = document.createElement('div');
-            charElement.classList.add('sw_char');
+        movies.Search.forEach(movie => {
+            const movieElement = document.createElement('div');
+            movieElement.classList.add('sw_char');
             
-            charElement.innerHTML = `
-                <h3>${character.name}</h3>
-                <p>Height: ${character.height} cm</p>
-                <p>Mass: ${character.mass} kg</p>
+            movieElement.innerHTML = `
+                <h3>${movie.Title}</h3>
+                <p>Year: ${movie.Year}</p>
+                <p>Type: ${movie.Type}</p>
             `;
             
-            swCharList.appendChild(charElement);
+            swCharList.appendChild(movieElement);
         });
         document.getElementById('loader').style.display = "none";
     }
 
-    function loadNextPage() {
-        fetchSwapiData(currentPage + 1)
-            .then(response => {
-                if (response.next !== null) {
-                    currentPage++;
-                    displayCharacters(response);
-                } else {
-                    nextPageButton.style.backgroundColor = "red";
-                }
-            });
-    }
-
-    function firstPage() {
-        currentPage = 1;
-        fetchSwapiData(currentPage)
-            .then(displayCharacters);
-    }
-    firstPage();
-
     function loadPreviousPage() {
         if (currentPage > 1) {
-            fetchSwapiData(currentPage - 1)
-                .then(response => {
-                    currentPage--;
-                    displayCharacters(response);
-                });
-        } else {
-            lastPageButton.style.backgroundColor = "red";
+            currentPage--;
+            fetchOmdbData(currentPage)
+                .then(displayMovies);
         }
+    }
+
+    function loadNextPage() {
+        currentPage++;
+        fetchOmdbData(currentPage)
+            .then(displayMovies);
     }
 
     lastPageButton.addEventListener('click', loadPreviousPage);
