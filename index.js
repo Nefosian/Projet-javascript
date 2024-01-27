989c9cefa52c73181bebd8b13d9dcb011e0e4695
Paul <paul.cornille-avot@supinfo.com>
2024-01-27 17:07:17 +0100

    ajout du systeme automatique pour placer les infos des films

--- index.js
+++ index.js
@@ -1,51 +1,34 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const movieElement = document.getElementById('poster1');
-    let currentPage = 1;
+document.addEventListener('DOMContentLoaded', async () => {
+    const moviesContainer = document.getElementById('moviesContainer');
 
-    function fetchData(search) {
-        swCharList.innerHTML = '';
-        let apiUrl = `http://www.omdbapi.com/?i=tt9218128`;
-        
-        if (search) {
-            apiUrl += `&s=${search}`;
-        }
-        
-        return fetch(apiUrl)
-            .then(response => response.json());
+    async function getMovieData(title) {
+        const apiUrl = `http://www.omdbapi.com/?apikey=c5ea3601&t=${title}&y=2024`;
+        const response = await fetch(apiUrl);
+        return response.json();
     }
 
-    function displayMovies(movies) {
-        swCharList.innerHTML = '';
-        if (movies.Search) {
-            movies.Search.forEach(movie => {
-                if (movie.Poster === "N/A"){
-                    movieElement.innerHTML = `
-                    <p>L'image ne peux pas être charger</p>
-                    <h3>${movie.Title}</h3>
-                `;
-                } else {
-                movieElement.innerHTML = `
-                    <img src="${movie.Poster}" alt="${movie.Title}">
-                    <h3>${movie.Title}</h3>
-                `;
-                }
-                swCharList.appendChild(movieElement);
-                nextPageButton.style.visibility = "visible";
-                lastPageButton.style.visibility = "visible";
-            })
-        } else {
-            swCharList.innerHTML = `
-            <p>Aucun résultat</p>
-            `;
-            nextPageButton.style.visibility = "hidden";
-            lastPageButton.style.visibility = "hidden";
-        }
-        document.getElementById('loader').style.display = "none";
+    function addMovieToContainer(movieData) {
+        const poster = document.createElement('div');
+        poster.className = 'poster';
+        poster.innerHTML = `
+            <img src="${movieData.Poster}" alt="poster1">
+            <div class="poster_description">
+                <p>${movieData.Plot}</p>
+                <div class="en_savoir_plus">
+                <a href="#">En savoir plus</a>
+                </div>
+            </div>
+            <p>${movieData.Title}</p>
+        `;
+        moviesContainer.appendChild(poster);
     }
 
-    displayMovies(fetchData(currentPage));
+    const movieTitles = ['Gladiator 2', 'Furiosa: A Mad Max Saga', 'Civil War', 'Godzilla x Kong', 'road house'];
 
-    lastPageButton.addEventListener('click', loadPreviousPage);
-    nextPageButton.addEventListener('click', loadNextPage);
-    searchButton.addEventListener('click', recupSearch);
+    for (const title of movieTitles) {
+        const movieData = await getMovieData(title);
+        if (movieData) {
+            addMovieToContainer(movieData);
+        }
+    }
 });
