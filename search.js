6ecd3ee724a92e8d5c45eb4c654ea3fc6426c9a1
Simon <simon.joubert@supinfo.com>
2024-01-28 10:09:31 +0100

    search en + sw_charlist -> filmList

--- search.js
+++ search.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const swCharList = document.getElementById('liste_films');
+    const filmList = document.getElementById('liste_films');
     const nextPageButton = document.getElementById('next_page');
     const lastPageButton = document.getElementById('previous_page');
     const searchButton = document.getElementById('searchButton');
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     function fetchData(page, search) {
         document.getElementById('loader').style.display = "block";
-        swCharList.innerHTML = '';
+        filmList.innerHTML = '';
         let apiUrl = `http://www.omdbapi.com/?page=${page}&apikey=c5ea3601`;
 
         if (search) {
@@ -20,22 +20,22 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     function displayMovies(movies) {
-        swCharList.innerHTML = '';
+        filmList.innerHTML = '';
     
         if (movies.Search && movies.Search.length > 0) {
             movies.Search.forEach(movie => {
                 const movieElement = document.createElement('div');
-                movieElement.classList.add('sw_char');
+                movieElement.classList.add('film');
                 movieElement.innerHTML = `
                     <img src="${movie.Poster}" alt="Le Poster ne peut pas être chargé">
                     <h3>${movie.Title}</h3>
                 `;
-                swCharList.appendChild(movieElement);
+                filmList.appendChild(movieElement);
                 nextPageButton.style.visibility = "visible";
                 lastPageButton.style.visibility = "visible";
             });
         } else {
-            swCharList.innerHTML = `
+            filmList.innerHTML = `
                 <p>Veuillez effectuer une recherche.</p>
             `;
             nextPageButton.style.visibility = "hidden";
