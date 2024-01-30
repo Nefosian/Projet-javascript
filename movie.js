a532d0d7e3125a98103092793ebad89fb4291bb8
Paul <paul.cornille-avot@supinfo.com>
2024-01-30 13:39:53 +0100

    ajour de lid pour afficher les films

--- movie.js
+++ movie.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const id = window.location.search.split('=')[1];
 
     function fetchData() {
-        let apiUrl = `http://www.omdbapi.com/?id=${id}&y=2024&plot=full&apikey=c5ea3601`;
+        let apiUrl = `http://www.omdbapi.com/?i=${id}&y=2024&plot=full&apikey=c5ea3601`;
         return fetch(apiUrl)
             .then(response => response.json());
     }
