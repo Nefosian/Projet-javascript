5ab872259817cd414d91c3c837717dc06ae4a96f
Paul <paul.cornille-avot@supinfo.com>
2024-01-30 16:46:58 +0100

    modification de tout le style refonte complete du site

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', async () => {
-    const moviesContainer = document.getElementById('moviesContainer');
+    const caseContainer = document.getElementById('case_container');
 
     async function getMovieData(title) {
         const apiUrl = `http://www.omdbapi.com/?apikey=c5ea3601&t=${title}&y=2024`;
@@ -9,21 +9,21 @@ document.addEventListener('DOMContentLoaded', async () => {
 
     function addMovieToContainer(movieData) {
         const poster = document.createElement('div');
-        poster.className = 'poster';
+        poster.className = 'case';
         poster.innerHTML = `
-            <img src="${movieData.Poster}" alt="poster1">
-            <div class="poster_description">
+            <img src="${movieData.Poster}" alt="poster" class="movie-poster">
+            <div class="poster-description">
                 <p>${movieData.Plot}</p>
-                <div class="en_savoir_plus">
-                <a href="#">En savoir plus</a>
+                <div class="en-savoir-plus">
+                <a href="movie.html?id=${movieData.imdbID}">En savoir plus</a>
                 </div>
             </div>
-            <p>${movieData.Title}</p>
+            <p class='movie-title'>${movieData.Title}</p>
         `;
-        moviesContainer.appendChild(poster);
+        caseContainer.appendChild(poster);
     }
 
-    const movieTitles = ['Blade', 'Furiosa: A Mad Max Saga', 'Civil War', 'Godzilla x Kong', 'Alien Romulus'];
+    const movieTitles = ['Blade', 'Furiosa: A Mad Max Saga', 'Civil War', 'Godzilla x Kong'];
 
     for (const title of movieTitles) {
         const movieData = await getMovieData(title);
@@ -32,42 +32,6 @@ document.addEventListener('DOMContentLoaded', async () => {
         }
     }
 
-
-    const gameMenuContent = document.querySelector('.game-menu-content');
-    const prevButton = document.querySelector('.prev-button');
-    const nextButton = document.querySelector('.next-button');
-
-    let scrollAmount = 0;
-    const cardWidth = 1993;
-
-    prevButton.addEventListener('click', () => {
-        scrollAmount += cardWidth;
-        gameMenuContent.style.transform = `translateX(${scrollAmount}px)`;
-        updateButtonState();
-    });
-
-    nextButton.addEventListener('click', () => {
-        scrollAmount -= cardWidth;
-        gameMenuContent.style.transform = `translateX(${scrollAmount}px)`;
-        updateButtonState();
-    });
-
-    function updateButtonState() {
-        prevButton.disabled = scrollAmount >= 0;
-        nextButton.disabled = scrollAmount <= -6000;
-    }
-
-    window.addEventListener('scroll', () => {
-        const navbar = document.querySelector('.navbar');
-        const scrollPosition = window.scrollY;
-
-        if (scrollPosition > 5000) {
-            navbar.classList.add('scrolled');
-        } else {
-            navbar.classList.remove('scrolled');
-        }
-    });
-
-    updateButtonState();
+    
 
 });
