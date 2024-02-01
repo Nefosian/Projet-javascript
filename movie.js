b12019247f0699b4facb1b8acde275d7cc71feb5
Paul <paul.cornille-avot@supinfo.com>
2024-02-01 02:13:03 +0100

    modification de style et de js pour affocher la plupart des choses manque plus que les bonus

--- movie.js
+++ movie.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     const poster = document.getElementById('moviesContainer');
+    const body = document.querySelector('body');
     const id = window.location.search.split('=')[1];
 
     function fetchData() {
@@ -10,12 +11,27 @@ document.addEventListener('DOMContentLoaded', function () {
     
     function renderData(data) {
         poster.innerHTML = `
-        <div class="movie">
-            <img src="${data.Poster}" alt="${data.Title}">
-            <h2>${data.Title}</h2>
-            <p>${data.Plot}</p>
+        <div class="movie-img" style="background-image: url('${data.Poster}');"></div>
+        <div class="movie-sub-image">
+            <div class="info">
+                <h1>${data.Title}</h1>
+                <ul class="movie-info">
+                    <li>Runtime : ${data.Runtime} <br> Genre : ${data.Genre}</li>
+                </ul>
+            </div>
+            <div class="summary">
+                <h5>SUMMARY :</h5>
+                <p>${data.Plot}</p>    
+            </div>
+            <div class="actors">
+                <h5>ACTORS :</h5>
+                <p>${data.Actors}</p>
+            </div>
         </div>
         `;  
+        const body = document.body;
+        const url = data.Poster;
+        body.style.backgroundImage = `linear-gradient(rgba(30, 27, 38, 0.95), rgba(30, 27, 38, 1)),url(${url})`
     }
 
     fetchData().then(renderData);
