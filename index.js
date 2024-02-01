0863d21efa00c601a063a3477f49a06b4e12bfd9
Paul <paul.cornille-avot@supinfo.com>
2024-02-01 14:28:59 +0100

    ajout du show more

--- index.js
+++ index.js
@@ -7,23 +7,30 @@ document.addEventListener('DOMContentLoaded', async () => {
         return response.json();
     }
 
-    function addMovieToContainer(movieData) {
+    function addMovieToContainer(movieData, isShowMore = false) {
         const poster = document.createElement('div');
         poster.className = 'case';
+
+        // Ajoutez une classe spécifique pour les éléments "Show more"
+        if (isShowMore) {
+            poster.classList.add('show-more');
+        }
+
         poster.innerHTML = `
             <img src="${movieData.Poster}" alt="poster" class="movie-poster">
             <div class="poster-description">
                 <p>${movieData.Plot}</p>
                 <div class="en-savoir-plus">
-                <a href="movie.html?id=${movieData.imdbID}">En savoir plus</a>
+                    <a href="movie.html?id=${movieData.imdbID}">En savoir plus</a>
                 </div>
             </div>
             <p class='movie-title'>${movieData.Title}</p>
         `;
+
         caseContainer.appendChild(poster);
     }
 
-    const movieTitles = ['Blade', 'Furiosa: A Mad Max Saga', 'Civil War', 'Godzilla x Kong'];
+    const movieTitles = ['Blade', 'Furiosa: A Mad Max Saga', 'Civil War', 'Godzilla x Kong']; 
 
     for (const title of movieTitles) {
         const movieData = await getMovieData(title);
@@ -32,6 +39,27 @@ document.addEventListener('DOMContentLoaded', async () => {
         }
     }
 
-    
+    const button = document.getElementById('show');
+    const movieTitles_show_more = ['The Beekeeper', 'Badland Hunters', 'Kingdom of the Planet of the Apes', 'Horizon'];
 
+    const showmore = async () => {
+        if (button.innerHTML === 'Show more') {
+            for (const titles of movieTitles_show_more) {
+                const movieDatas = await getMovieData(titles);
+                if (movieDatas) {
+                    addMovieToContainer(movieDatas, true);
+                }
+            }
+            button.innerHTML = 'Show less';
+        } else if (button.innerHTML === 'Show less') {
+            const showMoreElements = document.querySelectorAll('.show-more');
+            showMoreElements.forEach(element => {
+                element.remove();
+            });
+
+            button.innerHTML = 'Show more';
+        }
+    };
+
+    button.addEventListener('click', showmore);
 });
